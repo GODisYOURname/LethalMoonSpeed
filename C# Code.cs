bd8ffe08a1f75a9c80b473ff1df1ee8250eed765
GODisYOURname <154090884+GODisYOURname@users.noreply.github.com>
2023-12-17 21:18:21 +0100

    Lethal Company Mod
    -Run fast
    -Jump high
    -Gravity low
    -Fall damage does not always happen.
    -Run mid air
    
    I think every on need to install it.
    Simply drag and drop the dll file into the BeplnEx plug-in folder.
    
    have fun.

---
+++ C# Code.cs
@@ -0,0 +1,43 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using SunicMod.Patches;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SunicMod
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class SunicModBase : BaseUnityPlugin
+    {
+        private const string modGUID = "Poseidon.SunicMod";
+        private const string modName = "Sunic Mod";
+        private const string modVersion = "1.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        private static SunicModBase Instance;
+
+        internal ManualLogSource mls;
+
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+
+            mls.LogInfo("This test has awake :) ");
+
+            harmony.PatchAll(typeof(SunicModBase));
+            harmony.PatchAll(typeof(PlayerControllerBPatch));           
+        }
+
+        
+    }
+}
---
+++ PlayerControllerBPatch.cs
@@ -0,0 +1,38 @@
+﻿using GameNetcodeStuff;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SunicMod.Patches
+{
+    [HarmonyPatch(typeof(PlayerControllerB))]
+    internal class PlayerControllerBPatch
+    {
+        [HarmonyPatch("Update")]
+        [HarmonyPostfix]
+        static void infiniteSprintPatch(ref float ___sprintMeter, ref float ___sprintMultiplier, ref float ___jumpForce, ref bool ___isFallingFromJump, ref bool ___isFallingNoJump, ref bool ___isSprinting, ref bool ___takingFallDamage) 
+        { 
+            ___sprintMeter = 1f;
+            ___jumpForce = 100f;
+            ___takingFallDamage = false;
+            ___isFallingFromJump = false;
+            ___isFallingNoJump = false;
+
+
+            if (___isSprinting == true)
+            {
+                ___sprintMultiplier = 20f;
+                 
+            }  
+
+        }
+        static void ChackingFingerPatch(ref float ___limpMultiplier)
+        {
+            ___limpMultiplier = 100f;
+        }
+    }
+}
